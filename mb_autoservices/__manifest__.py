8fd8c9f5c425d75d804961abd83c9188c271497c
Marcos Bolivar <66689110+captivea-mbo@users.noreply.github.com>
2020-08-30 21:29:30 -0400

    update

---
+++ mb_autoservices/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "KnowSystem",
+    "version": "12.0.1.0.19",
+    "category": "Extra Tools",
+    "author": "Odoo Tools",
+    "website": "https://odootools.com/apps/12.0/knowsystem-287",
+    "license": "Other proprietary",
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+    "depends": ["mail","web_editor"],
+    "data": [
+    ]
+
+}
