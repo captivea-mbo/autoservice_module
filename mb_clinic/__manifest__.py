af31867c0b31e061114158ee42590eacda6082c0
Marcos Bolivar <66689110+captivea-mbo@users.noreply.github.com>
2020-08-31 09:04:56 -0400

    update

---
+++ mb_clinic/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Odoo Development Tutorials',
+    'version': '12.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'Odoo Development Tutorials For Beginners',
+    'sequence': '10',
+    'license': 'AGPL-3',
+    'author': 'Odoo Mates',
+    'maintainer': 'Odoo Mates',
+    'website': 'www.google.com',
+    'depends': ['sale', 'mail', 'report_xlsx', 'web_timeline'],
+    'demo': [],
+    'data': [
+    ],
+    'images': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
+# Video Explanation: https://www.youtube.com/watch?v=BDepk0LhVuI&list=PLqRRLx0cl0hoJhjFWkFYowveq2Zn55dhM&index=1
